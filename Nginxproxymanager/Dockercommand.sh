6a4201ea028bdc2848f0cd8c9b7fe00ee0ca90e5
Brainson77 <ttdickson86@gmail.com>
2025-05-21 08:09:55 -0400

    commit code

--- Nginxproxymanager/Dockercommand.sh
+++ Nginxproxymanager/Dockercommand.sh
@@ -1,3 +1,5 @@
 docker build -t ntosah77-nginxproxy:latest .
 docker push ttdickson86/ntosah77portainer:tagname
 docker run -d --name ccf11a8ffd9c -p 8080:80 ntosah77-nginxproxy:latest
+
+Link: https://medium.com/nerd-for-tech/deploy-a-custom-nginx-docker-image-and-push-it-to-docker-hub-118f1ab2186b
