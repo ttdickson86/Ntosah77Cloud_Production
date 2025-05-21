11fb77ad53481b576acbfff6b9bb75d05e7351c4
Brainson77 <ttdickson86@gmail.com>
2025-05-21 17:21:19 -0400

    update

--- Nginxproxymanager/Dockercommand.sh
+++ Nginxproxymanager/Dockercommand.sh
@@ -1,5 +1,7 @@
 docker build -t ntosah77-nginxproxy:latest .
-docker push ttdickson86/ntosah77portainer:tagname
+
+docker push ttdickson86/ntosah77portainer:ntosah77-nginxproxy:latest
+
 docker run -d --name ccf11a8ffd9c -p 8080:80 ntosah77-nginxproxy:latest
 
 Link: https://medium.com/nerd-for-tech/deploy-a-custom-nginx-docker-image-and-push-it-to-docker-hub-118f1ab2186b
